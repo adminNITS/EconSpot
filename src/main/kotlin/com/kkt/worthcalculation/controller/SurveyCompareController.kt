e2b7fe09f870332dcc6f0ddf884c02ccda7d3e00
Kriangkrai Prommaithong <kr.prommaithong@gmail.com>
2022-07-18 21:09:24 +0700

    Feat : revise endpoint

--- src/main/kotlin/com/kkt/worthcalculation/controller/SurveyCompareController.kt
+++ src/main/kotlin/com/kkt/worthcalculation/controller/SurveyCompareController.kt
@@ -11,12 +11,12 @@ import java.time.LocalDateTime
 import javax.validation.Valid
 
 
-@RequestMapping("api/v2/surveyCompare")
+@RequestMapping("api/v2/worth")
 @RestController
 class SurveyCompareController(val service: SurveyCompareService) {
     private val logger = LoggerFactory.getLogger(javaClass.name)
 
-    @PostMapping("/import")
+    @PostMapping("/excel/import")
     fun importExcel(@Valid @RequestParam sportTournamentId: String, @RequestParam("uploadfile") file: MultipartFile): ResponseEntity<ResponseModel> {
         logger.info("request: $sportTournamentId ${file.originalFilename}")
         if (file.contentType != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
@@ -49,13 +49,29 @@ class SurveyCompareController(val service: SurveyCompareService) {
         return ResponseEntity.ok(response)
     }
 
-    @GetMapping("/")
+
+    @GetMapping("/excel/download")
+    fun downloadExcel(@RequestParam sportTournamentId: String, @RequestParam excelId: String): ResponseEntity<ResponseModel> {
+        logger.info("request: $sportTournamentId, $excelId")
+
+        val response: ResponseModel = ResponseModel(
+                message = "Import data success",
+                status = "ok",
+                timestamp = LocalDateTime.now(),
+                data = null,
+                pagination = null
+            )
+
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/excel")
     fun getListImport(@Valid @RequestParam sportTournamentId: String): ResponseEntity<ResponseModel> {
         logger.info("request: $sportTournamentId")
         return service.getListImport(sportTournamentId)
     }
 
-    @GetMapping("/compare")
+    @PostMapping("/excel/compare")
     fun getCompare(@Valid @RequestBody requestModel: RequestCompareCriteria): ResponseEntity<ResponseModel> {
         logger.info("request Tournament A: ${requestModel.tournamentA.tournamentId}")
         logger.info("request Tournament B: ${requestModel.tournamentB.tournamentId}")
